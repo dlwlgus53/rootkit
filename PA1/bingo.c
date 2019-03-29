14d9d429701ff0765aba4686826a4ad9a94c4951
이지현 <dlwlgus4553@gmail.com>
2019-03-29 08:08:35 -0700

    bingo

--- PA1/bingo.c
+++ PA1/bingo.c
@@ -10,7 +10,7 @@ int main (int argc, char *argv[]){
 	
 
 	while(strcmp(input, "quit")!= 0){
-		printf("choose the function\n");
+		printf("choose the function(input number)\n");
 		printf("--1. input userid\n");
 		printf("--2. get user log\n");
 		printf("--3. make imortal/mortal process\n");
@@ -18,9 +18,28 @@ int main (int argc, char *argv[]){
 		printf("----------------------\n");
 		printf(">");
 		scanf ("%s", input);
-		if(strcmp(input,"clear") == 0){
-			printf("c\n");
+		if(strcmp(input,"1") == 0){
+			printf("1\n");
+		}else if(strcmp(input,"2") == 0){
+			printf("2\n");
+		}else if(strcmp(input,"3") == 0){
+			printf("3\n");
+		}else if(strcmp(input,"4") == 0){
+			printf("4\n");
+		}else{
+			printf("no such function\n");
 		}
+		
+
 	}
 
  }
+
+void hello_write(void) 
+{
+	FILE *file_pointer;
+	file_pointer=fopen("/proc/openhook3", "w");
+	fprintf(file_pointer, "bingo.c");
+	fclose(file_pointer);
+	return  ;
+}
